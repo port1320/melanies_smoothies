a57b294b39c397ad50b9e914709f41ace558d765
port1320 <155856485+port1320@users.noreply.github.com>
2024-05-11 17:26:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,14 +33,11 @@ if ingredients_list:
 		fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 		my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            	values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+        values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 		
-    			time_to_insert = st.button('Submit Order')
+time_to_insert = st.button('Submit Order')
     			
-				if time_to_insert:
-        				session.sql(my_insert_stmt).collect()  
-        				st.success('Your Smoothie is ordered, ' + name_on_order + "!", icon="✅")
-
-
-
-
+if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered, ' + name_on_order + "!", icon="✅")
+    
