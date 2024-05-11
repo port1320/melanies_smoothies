74128c26c770078686f94bc0d47c85f052506017
port1320 <155856485+port1320@users.noreply.github.com>
2024-05-11 17:14:52 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,6 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-#option = st.selectbox(
-#    "What is your favorite fruit?",
-#    ("Banana", "Strawberries", "Peaches"))
-
 #st.write("You selected:", option)
 cnx = st.connection("snowflake")
 session = cnx.session()
@@ -35,22 +31,15 @@ if ingredients_list:
 		ingredients_string += fruit_chosen + ' '
 		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 		fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-    #st.write(ingredients_string)
 
 		my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             	values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-
-    #st.write(my_insert_stmt)
-    #st.stop()
 
     		time_to_insert = st.button('Submit Order')
 
     		if time_to_insert:
         		session.sql(my_insert_stmt).collect()  
         		st.success('Your Smoothie is ordered, ' + name_on_order + "!", icon="✅")
-
-#New section to display fruityvice nutrition information
-#st.text(fruityvice_response.json())
 
 
 
